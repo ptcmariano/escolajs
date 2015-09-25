9cf0a5249d6d0c44db2b8ea3b23b881b008d746c
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2015-09-25 13:58:58 -0300

    Criada ação no controlador Cursos para criação de novo curso e definição da área a qual ele pertence.

--- controllers/api/cursosController.js
+++ controllers/api/cursosController.js
@@ -2,9 +2,19 @@ var Curso = require('../../models/Curso');
 var ControladorRest = require('../../components/ControladorRest'),
     controller = new ControladorRest(Curso);
 
-exports.novoCurso = controller.novaInstancia.bind(controller);
 exports.exibirCurso = controller.exibirInstancia.bind(controller);
 exports.editarCurso = controller.editarInstancia.bind(controller);
 exports.excluirCurso = controller.excluirInstancia.bind(controller);
 exports.listarCursos = controller.listarInstancias.bind(controller);
 exports.obterCursoMiddleware = controller.carregarInstanciaPorId.bind(controller);
+
+exports.novoCurso = function(req, res, next) {
+    return Curso.novaInstancia(req.body)
+        .then(function(curso) {
+            return curso.setArea(req.body.area);
+        })
+        .then(function(curso) {
+            res.json(curso);
+        })
+        .catch(next);
+};
