3e29847eaa00a3c1761ad2a797fab0638753a5f4
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2015-09-25 13:58:17 -0300

    Estendida instância do modelo Curso com o método setArea para definir a Área a qual o curso pertence.

--- models/Curso.js
+++ models/Curso.js
@@ -1,6 +1,21 @@
 var sequelize = require('../config/sequelize').getSequelize(),
+    Area = require('../components/ModeloBase')(sequelize.model('Area')),
     Curso = require('../components/ModeloBase')(sequelize.model('Curso'));
 
 Curso.definirCamposAtualizaveis(['curso', 'sigla']);
+Curso.estenderInstancia('setArea', function(idArea) {
+    var instanciaCurso = this;
+
+    return Area.obterPorId(idArea)
+        .then(function(area) {
+            return instanciaCurso.dbInstancia.setArea(area.dbInstancia);
+        })
+        .then(function(dbCurso) {
+            return Curso.criarInstancia(dbCurso);
+        })
+        .catch(function(error) {
+            throw error;
+        });
+});
 
 module.exports = Curso;
